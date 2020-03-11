f85a54c81ad0110391aba3d3d7ee609896a75597
ssc-bando <satoshi_bando@solver.co.jp>
2020-03-11 17:13:30 +0900

    initial state change

---
+++ app/src/main/java/com/example/customerapplicationbase/LoginProgressActivity.java
@@ -0,0 +1,14 @@
+package com.example.customerapplicationbase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginProgressActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_progress);
+    }
+}
