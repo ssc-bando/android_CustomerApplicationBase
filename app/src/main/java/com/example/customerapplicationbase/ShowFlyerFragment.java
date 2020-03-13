9f24b2ee75113bffafed1e3afe14942b71f274e0
ssc-bando <satoshi_bando@solver.co.jp>
2020-03-13 10:01:22 +0900

    add: load sample image on WebView of Flyer

--- app/src/main/java/com/example/customerapplicationbase/ShowFlyerFragment.java
+++ app/src/main/java/com/example/customerapplicationbase/ShowFlyerFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 
 /**
@@ -23,6 +24,9 @@ public class ShowFlyerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_show_flyer, container, false);
+        View root = inflater.inflate(R.layout.fragment_show_flyer, container, false);;
+        WebView flyer = (WebView)root.findViewById(R.id.webview_flywer);
+        flyer.loadUrl("https://1.bp.blogspot.com/-j8vui1ZaN0Y/XhwqpvcxctI/AAAAAAABXCc/tfOSJ359HKgexOulf_j1Irp4ROPtNG8EgCNcBGAsYHQ/s400/shinbun_orikomi_koukoku_ad.png");
+        return root;
     }
 }
